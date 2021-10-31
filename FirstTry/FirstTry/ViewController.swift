465cae2a00bd5acb5dc39525636214a611c19d25
Ъ <55149626+ni-k-azakov@users.noreply.github.com>
2021-10-31 15:19:48 +0300

    :tada: init

---
+++ FirstTry/FirstTry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstTry
+//
+//  Created by Nikita Kazakov on 28.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
