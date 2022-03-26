7d9081cbd69f61c5671b29802208a178335b1e1b
Ъ <55149626+ni-k-azakov@users.noreply.github.com>
2022-03-26 18:26:44 +0300

    mem

---
+++ FeedMe/FeedMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeedMe
+//
+//  Created by Nikita Kazakov on 26.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- SwiftTest.playground/Contents.swift
+++ SwiftTest.playground/Contents.swift
@@ -1,14 +1,56 @@
-import UIKit
+import Foundation
 
-var array: [[String : String]] = []
-var dict = ["mem" : "mom", "kek" : "kok"]
-array.append(dict)
-print(dict)
-print(array)
-dict.removeAll()
-print(dict)
-print(array)
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
 
-print(NSDate(timeIntervalSince1970: NSDate().timeIntervalSince1970 + Double(TimeZone.current.secondsFromGMT())))
-
-
+class Solution {
+    static func reverseKGroup(_ head: ListNode?, _ k: Int) -> ListNode? {
+        var tempNode = head
+        var prevNode: ListNode? = nil
+        var prevBase: ListNode? = nil
+        var prevPrevBase: ListNode? = nil
+        var finalHead: ListNode? = nil
+        var nodesAmount = 0
+        while let node = tempNode {
+            nodesAmount += 1
+            tempNode = node.next
+        }
+        tempNode = head
+        for i in 0..<nodesAmount - nodesAmount % k {
+            let node = tempNode!
+            tempNode = node.next
+            if i % k == 0 {
+                prevPrevBase = prevBase
+                prevBase = node
+            } else {
+                if i % k == k - 1 {
+                    if let unwrapped = prevPrevBase {
+                        unwrapped.next = node
+                    } else {
+                        finalHead = node
+                    }
+                }
+                node.next = prevNode
+            }
+            prevNode = node
+        }
+        if nodesAmount % k != 0 {
+            prevBase?.next = tempNode
+        }
+        if let unwrapped = finalHead {
+            return unwrapped
+        } else {
+            return head
+        }
+    }
+}
+var node = Solution.reverseKGroup(ListNode(1, ListNode(2)), 2)
+while let _ = node {
+    print(node!.val)
+    node = node!.next
+}
