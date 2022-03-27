23f9dde1e0f05867406b987c9bc80283a7dd6163
Ъ <55149626+ni-k-azakov@users.noreply.github.com>
2022-03-27 20:36:49 +0300

    fixed cap

--- FeedMe/FeedMe/ViewController.swift
+++ FeedMe/FeedMe/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cardInfo: UIView!
+    @IBOutlet weak var cardCheqeue: UIView!
+    @IBOutlet weak var cardScroll: UIScrollView!
+    @IBOutlet weak var openButton: UIButton!
+    @IBOutlet weak var closeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupCard()
     }
-
-
+    
+    private func setupCard() {
+        openButton.isHidden = false
+        closeButton.isHidden = true
+    }
+    
+    @IBAction func openCard(_ sender: UIButton) {
+        scrollToPage(page: 1, animated: true)
+        openButton.isHidden = true
+        closeButton.isHidden = false
+    }
+    
+    @IBAction func closeCard(_ sender: UIButton) {
+        scrollToPage(page: 0, animated: true)
+        openButton.isHidden = false
+        closeButton.isHidden = true
+    }
+    
+    private func scrollToPage(page: Int, animated: Bool) {
+        var frame: CGRect = cardScroll.frame
+        frame.origin.x = frame.size.width * CGFloat(page)
+        frame.origin.y = 0
+        cardScroll.scrollRectToVisible(frame, animated: animated)
+    }
 }
 
