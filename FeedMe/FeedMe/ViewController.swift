aca41879a570f9b566849649dbdcd1135f574bb6
Ъ <55149626+ni-k-azakov@users.noreply.github.com>
2022-04-17 00:03:59 +0300

    :sparkles: Update: added schedule + table view

--- FeedMe/FeedMe/ViewController.swift
+++ FeedMe/FeedMe/ViewController.swift
@@ -13,10 +13,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var cardScroll: UIScrollView!
     @IBOutlet weak var openButton: UIButton!
     @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var scheduleTable: UITableView!
+    
+    let orders = [
+        "22.10 18:30 Травы, самовывоз",
+        "29.10 15:00 Продукты, доставка",
+        "01.11 14:50 Teremok, доставка",
+        "12.11 17:38 KFC, самовывоз",
+        "13.11 08:30 Макдудлс, доставка",
+        "18.11 09:50 Вкусная еда, самовывоз",
+        "02.12 18:00 ЖОПАиЖОПА, доставка"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCard()
+        scheduleTable.delegate = self
+        scheduleTable.dataSource = self
     }
     
     private func setupCard() {
@@ -44,3 +57,20 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("NIKAZ: TAPPED")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return orders.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "default", for: indexPath)
+        cell.textLabel?.text = orders[indexPath.row]
+        return cell
+    }
+}
